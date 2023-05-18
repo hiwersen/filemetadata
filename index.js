10ad899e6ec9dddba798cd761b5fb53037a75799
Hiwersen Angelo Gnocchi Godoy <100746220+hiwersen@users.noreply.github.com>
2023-05-18 14:32:19 +0100

    feat: Implement security features for file uploads
    - Replace GridFS with Multer memoryStorage for file storage
    - Implement MIME type allow-list for basic file type validation
    - Use file-type library to verify actual file content matches claimed MIME type
    - Restrict upload file size using Multer's limit option

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
 
 /**
  * Import Multer.
@@ -14,22 +13,9 @@ const mongoose = require('mongoose');
  */
 const multer = require('multer');
 
-/**
- * Import Multer GridFS Storage.
- * 
- * Multer GridFS Storage is a GridFS storage engine for Multer to store uploaded files directly to MongoDb.
- * GridFS is a specification in MongoDB for storing and retrieving large files  such as images, audio files, video files, etc. 
- * This means that GridFsStorage allows Multer to save uploaded files directly in MongoDB in a way that's efficient for large files.
- * 
- * @see {@link https://www.npmjs.com/package/multer-gridfs-storage?activeTab=readme}
- */
-const { GridFsStorage } = require('multer-gridfs-storage');
-
 const app = express();
 
 const port = process.env.PORT || 3000;
-const dbUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/fileMetadata';
-module.exports = dbUri;
 
 /**
  * Enable CORS (Cross-origin resource sharing) middleware
@@ -37,20 +23,6 @@ module.exports = dbUri;
  * @see {@link https://www.npmjs.com/package/cors}
  */
 app.use(cors());
-
-// Establish a connection to the MongoDB database using Mongoose with specified options,
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true, });
-
-// Retrieve the instance of the active MongoDB connection managed by Mongoose.
-const connection = mongoose.connection;
-
-// Log the connection error to the console, upon 'error' event.
-connection.on('error', console.error.bind(console, 'connection error:'));
-
-// Log the database name to the console, upon 'open' event.
-connection.once('open', () => {
-  console.log(`Connected to database: ${connection.name}`); // also: connection.db.databaseName
-});
 
 /**
  * Serve static files (CSS, JavaScript, images) from the 'public' directory
@@ -64,8 +36,13 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Instanciate Multer object with the specified storage configuration.
-const upload = multer({ storage: multer.memoryStorage() });
+// Instanciate Multer object with the specified storage limits options
+const upload = multer({ 
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 1 * 1024 * 1024, // limit file size to 1MB
+  },
+});
 
 /**
  * Security feature:
@@ -86,25 +63,71 @@ const allowedMimeTypes = [
   'video/mp4'    // For MP4 video files
 ];
 
-app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  // req.file.originalname = Name of the file on the user's computer VS 
-  // req.file.filename = The name of the file as stored on the server
-  // @see {@link https://github.com/expressjs/multer#file-information}
-  const { originalname: name, mimetype: type, size } = req.file;
-
-  // Check if the file's MIME type is in the allow-list
-  if (!allowedMimeTypes.includes(type)) {
-    return res.status(400).send('Invalid file type');
-  }
-
-  // Send a JSON object with the file name, type and size in bytes metadata
-  res.json({ name, type, size });
-});
-
 /**
- * Start the server and log the listening port
+ * Dynamically import file-type.
+ * 
+ * The file-type package is for detecting binary-based file formats, not text-based formats like .txt, .csv, .svg, etc.
+ * 
+ * @see {@link https://www.npmjs.com/package/file-type}
  */
-const listener = app.listen(port, function () {
-  console.log('Listening on port ' + port)
+import('file-type').then(fileTypeModule => {
+
+  app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+    // req.file.originalname = Name of the file on the user's computer VS 
+    // req.file.filename = The name of the file as stored on the server
+    // @see {@link https://github.com/expressjs/multer#file-information}
+    const { originalname: name, mimetype: type, size, buffer } = req.file;
+  
+    // Check if the file's MIME type is in the allow-list
+    if (!allowedMimeTypes.includes(type)) {
+      return res.status(400).send('Invalid file type');
+    } else {
+
+  
+      // Determine the file type from a Buffer (file's actual content)
+      ({ fileTypeFromBuffer } = fileTypeModule);
+      const fileTypeResult = await fileTypeFromBuffer(buffer);
+
+      if (fileTypeResult) {
+        console.log("fileTypeFromBuffer worked: ");
+        console.log(fileTypeResult);
+        console.log("file tipe claimed: " + type);
+
+        const { mime: mimeTypeFromBuffer} = fileTypeResult;
+        if (!mimeTypeFromBuffer || mimeTypeFromBuffer !== type) {
+          return res.status(400).send('File content does not match MIME type');
+        } else {
+    
+        // Send a JSON object with the file name, type and size in bytes metadata
+        res.json({ name, type, size });
+        }
+      } else {
+        return res.status(400).send('Unable to determine file type from content');
+      }
+    }
+  });
+
+  app.use((err, req, res, next) => {
+    if (err instanceof multer.MulterError) {
+      // Special case for handling Multer errors
+      console.log('multer error');
+      return res.status(400).json({ error: err.message });
+    } else {
+      // Default case for handling other types of errors
+      return res.status(500).json({ error: 'An unknown error occurred while uploading the file.' });
+    }
+  });
+  
+  /**
+   * Start the server and log the listening port
+   */
+  const listener = app.listen(port, () => {
+    console.log('Listening on port ' + port)
+  });
+
+}).catch(err => {
+  console.error('Error loading dependencies:' + err);
+  process.exit(1);
 });
+
 
